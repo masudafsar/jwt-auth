43e3d93455473bab41432a143f19ae1cb6f75b19
Masud Afsar <masud.afsar@gmail.com>
2024-03-16 19:31:06 +0330

    Install and config swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as packageConfig from '../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('JWT Auth API')
+    .setDescription('Documentation for an API which provides authentication with jwt access token and refresh token')
+    .setVersion(packageConfig.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
+
 bootstrap();
