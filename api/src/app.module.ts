80e7c38e395eb25bc27dd2fe5d8a2f36519bb43f
Masud Afsar <masud.afsar@gmail.com>
2024-03-16 18:01:32 +0330

    Implement database config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import databaseConfig from "../config/database.config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRoot(databaseConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
