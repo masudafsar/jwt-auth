e5d2b1f3bb097bbf1500c6d231e9a663a3e619af
Masud Afsar <masud.afsar@gmail.com>
2024-03-19 01:43:33 +0330

    Implement database and data-source configs

---
+++ api/config/datasource.config.mjs
@@ -0,0 +1,29 @@
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+const datasource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER || 'root',
+  password: process.env.POSTGRES_PASSWORD || 'password',
+  database: process.env.POSTGRES_DB || 'main-db',
+  entities: ['dist/**/*.entity.{ts,js}'],
+  migrations: ['dist/**/migrations/*.{ts,js}'],
+  migrationsTableName: 'TypeORM_Migrations',
+  synchronize: false,
+  migrationsRun: false,
+  logging: true,
+  logger: 'advanced-console',
+});
+
+datasource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((reason) => {
+    console.log('Error during Data Source initialization', reason);
+  });
+
+export default datasource;
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,20 +1,23 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import * as packageConfig from '../package.json'
+import * as process from 'process';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageConfig from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('JWT Auth API')
-    .setDescription('Documentation for an API which provides authentication with jwt access token and refresh token')
+    .setDescription(
+      'Documentation for an API which provides authentication with jwt access token and refresh token',
+    )
     .setVersion(packageConfig.version)
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3000);
 }
 
 bootstrap();
