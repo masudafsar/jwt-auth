5f2ccc41dab2c263e240ad053a5bb0440f7aa31d
Masud Afsar <masud.afsar@gmail.com>
2024-03-26 02:25:12 +0330

    Implement jwt strategies

---
+++ api/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import * as process from 'process';
+import { JwtPayloadType } from '../types/jwt-payload-type';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_ACCESS_SECRET,
+    });
+  }
+
+  validate(payload: JwtPayloadType) {
+    return payload;
+  }
+}
---
+++ api/src/auth/types/jwt-payload-type.ts
@@ -0,0 +1,4 @@
+export interface JwtPayloadType {
+  sub: string;
+  username: string;
+}
